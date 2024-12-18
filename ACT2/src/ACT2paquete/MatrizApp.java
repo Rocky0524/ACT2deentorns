7b4a1a664c70b76d57aa836276c027f69f18c8af
AlexFR0 <afrancesretamero50@gmail.com>
2024-12-18 19:55:19 +0100

    Matriz Inicial 1
    Aquí declaramos 2 variables y creamos una matriz de 5X5.
    
    Creamos un bucle FOR donde leemos tanto filas y columnas y generamos un num RANDOM hasta el 100.

--- ACT2/src/ACT2paquete/MatrizApp.java
+++ ACT2/src/ACT2paquete/MatrizApp.java
@@ -1,7 +1,26 @@
 package ACT2paquete;
 
+import java.util.Random;
+
 public class MatrizApp {
     public static void main(String[] args) {
         // TODO: Implementar las funciones según las ramas asignadas
+    	
+    	
+    	 int filas = 5;
+         int columnas = 5;
+         int[][] matriz = new int[filas][columnas];
+    	
+         Random random = new Random();
+
+         
+         for (int i = 0; i < filas; i++) {
+             for (int j = 0; j < columnas; j++) {
+                 matriz[i][j] = random.nextInt(100) + 1; //Num aleatorios
+             }
+         }
+    	
+         
+    	
     }
 }
