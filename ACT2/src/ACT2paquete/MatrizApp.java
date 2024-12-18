178a063dc78ffc9642b3968e150f4d91e74f61ab
Rocky0524 <kenbalboa05@gmail.com>
2024-12-18 19:49:36 +0100

    Revert "prueba"
    This reverts commit d0c0f5db943332de50c12cec6fa235c6c8760a48.

--- ACT2/src/ACT2paquete/MatrizApp.java
+++ ACT2/src/ACT2paquete/MatrizApp.java
@@ -3,7 +3,5 @@ package ACT2paquete;
 public class MatrizApp {
     public static void main(String[] args) {
         // TODO: Implementar las funciones según las ramas asignadas
-    	System.out.println("hola");
-    	
     }
 }
