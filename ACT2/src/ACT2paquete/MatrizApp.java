508a3feafb028ff6211c36e1291cd377def63096
Erlzo <ericromerogasol53@gmail.com>
2024-12-18 20:11:10 +0100

    Suma-matriz
    Borrado codigo duplicado + suma de toda la matriz implementada.

--- ACT2/src/ACT2paquete/MatrizApp.java
+++ ACT2/src/ACT2paquete/MatrizApp.java
@@ -6,33 +6,7 @@ import java.util.Random;
 public class MatrizApp {
     public static void main(String[] args) {
         // TODO: Implementar las funciones según las ramas asignadas
-    	
-    	
-    	 int filas = 5;
-         int columnas = 5;
-         int[][] matriz = new int[filas][columnas];
-    	
-         Random random = new Random();
 
-         
-         for (int i = 0; i < filas; i++) {
-             for (int j = 0; j < columnas; j++) {
-                 matriz[i][j] = random.nextInt(100) + 1; //Num aleatorios
-             }
-         }
-    	
-         System.out.println("Matriz de 5x5 de números aleatorios entre 1 y 100:");
-         for (int i = 0; i < filas; i++) {
-             for (int j = 0; j < columnas; j++) {
-                 System.out.print(matriz[i][j] + "\t"); 
-             }
-             System.out.println(); 
-         }
-         
-    	
-    }
-	public static void main(String[] args) {
-		// TODO: Implementar las funciones según las ramas asignadas
 
 		int filas = 5;
 		int columnas = 5;
@@ -74,5 +48,15 @@ public class MatrizApp {
 		if (!encontrado) {
 			System.out.println("El número " + numeroBuscado + " no se encuentra en la matriz.");
 		}
-	}
+		 // Calcular la suma de todos los números en la matriz
+        int suma = 0;
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                suma += matriz[i][j]; // Sumamos cada número
+            }
+        }
+
+        // Mostrar la suma
+        System.out.println("La suma de todos los números en la matriz es: " + suma);
+    }
 }
